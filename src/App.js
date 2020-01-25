0fa66e61c019036b451534a79f747a9f1e60d611
solar05 <artem00298@gmail.com>
2020-01-26 02:29:45 +0300

    Check new branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-        Learn React
+        Changed on a feature branch.
         </a>
       </header>
     </div>
